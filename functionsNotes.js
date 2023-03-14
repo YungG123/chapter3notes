142c324dbe52e1712d8dee6dcb2ee5be9a50bc17
pwj2 <pwj2@fresnou.org>
2023-03-14 14:00:08 -0700

    initial commit

---
+++ functionsNotes.js
@@ -0,0 +1,42 @@
+//Function Notes
+//Step-1 define the function
+// function sayHello(){
+//     console.log("Hello");
+// }
+//Step 2 - call (use) the function
+//sayHello();
+//sayHello();
+//sayHello();
+//sayHello();
+//
+// function printMax(){
+//     let x = 6;
+//     let y = 29;
+//     if (x>y){
+//         console.log(`the longer number is ${x}`);
+//     } else{
+//         console.log(`the longer number is ${x}`);
+//     }
+// }
+//
+// printmax();
+//
+// function printMax(x, y){
+//     if (x>y){
+//         console.log(`the longer number is ${x}`);
+//     }else {
+//         console.log(`the longer number is ${y}`);
+//     }
+// }
+//
+// printMax(8,21);
+// printMax(7,-21);
+// printMax(2,2);
+// printMax(-8,21);
+
+function sum(number1, number2){
+    console.log(`${number1} + ${number2} = ${number1+number2}`);
+}
+
+sum(7, 12);
+sum(0,5)
