6112eaeb789f424660e1698fde40ac652ee399ac
pwj2 <pwj2@fresnou.org>
2023-04-18 14:18:03 -0700

    initial commit

---
+++ arrayNotes2.js
@@ -0,0 +1,7 @@
+function count(n){
+    let myarr=[]
+    for(let i=1; i<=n; i++){
+    }
+    console.log(myArr);
+}
+
---
+++ arrays1.js
@@ -0,0 +1,9 @@
+//1
+function printFirst(arr){
+    console.log(arr[0])
+}
+
+printFirst([3, 12, 7, 14]);
+printFirst(['a','b','c','d']);
+
+//2
